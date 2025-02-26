dd7d6d9331727754ba7f8c0c68c868db5a82544e
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-02-26 14:14:39 -0600

    added player and side to side movment

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -9,16 +9,38 @@ let y = 0;
 let changeY = 1;
 let gravMulti = 1.03;
 let grounded = true;
+let playerSize = 30;
 
 
 function setup() {
   createCanvas(1280, 720);
-  y = 720;
+  y = height/2;
+  x = width/2;
 }
 
 function draw() {
+  //movment and change
+  playerMovment();
   gravity();
+
+
+  //drawing things like player and level
   background(255);
+  drawPlayer();
+}
+
+function playerMovment(){
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 5;
+  } 
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 5;
+  } 
+
+  if (keyIsDown(DOWN_ARROW)) {
+    playerSize = 20;
+  }
 }
 
 function gravity() {
@@ -28,6 +50,15 @@ function gravity() {
 
 }
 
-function drawPlayer(){
-  rect(x, y, 50);
+function drawPlayer() {
+  rect(x, y, 30, playerSize);
+  rect(x + 6, y + 6, 5);
+  rect(x + 19, y + 6, 5);
+}
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    playerSize = 30;
+  }
+
 }
