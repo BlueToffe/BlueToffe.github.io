b80a5b29583337763e69a77c65e71a9ff65f4f95
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-02-26 13:33:47 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -4,12 +4,30 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let x = 0;
+let y = 0;
+let changeY = 1;
+let gravMulti = 1.03;
+let grounded = true;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1280, 720);
+  y = 720;
 }
 
 function draw() {
-  background(220);
+  gravity();
+  background(255);
+}
+
+function gravity() {
+  if (y <= 720){
+
+  }
+
+}
+
+function drawPlayer(){
+  rect(x, y, 50);
 }
