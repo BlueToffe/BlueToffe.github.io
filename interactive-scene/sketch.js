6d23d9e07c6595fbe958f4091f16b458b276387b
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-03-03 14:13:59 -0600

    Block creation and Block collision start

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -4,13 +4,18 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+
 let x = 0;
 let y = 0;
 let changeY = 1;
 let gravMulti = 1.03;
 let grounded = false;
 let playerSize = 30;
-
+let blockLocationX = [150, 300, 600];
+let blockLocationY = [50, 120, 200];
+let blockSizeX = [50, 70, 80];
+let blockSizeY = [30, 40, 20];
 
 function setup() {
   createCanvas(1280, 720);
@@ -26,6 +31,7 @@ function draw() {
 
   //drawing things like player and level
   background(255);
+  drawBlocks();
   drawPlayer();
 }
 
@@ -45,12 +51,18 @@ function playerMovment() {
 
 function gravity() {
   if (grounded === false){
-    
+    //take the blockx and block size x and y and find if character is on those if so set character y to top of blockY
   }
+}
 
+function moveToSurface() {
+  if (grounded === true) {
+
+  }
 }
 
 function drawPlayer() {
+  fill(255);
   rect(x, y, 30, playerSize);
   rect(x + 6, y + 6, 5);
   rect(x + 19, y + 6, 5);
@@ -59,5 +71,12 @@ function drawPlayer() {
 function keyReleased() {
   if (keyCode === DOWN_ARROW) {
     playerSize = 30;
+  }
+}
+
+function drawBlocks() {
+  for (let block = 0; block <= blockLocationX.length; block += 1) {
+    fill(150);
+    rect(blockLocationX[block], blockLocationY[block], blockSizeX[block], blockSizeY[block]);
   }
 }
