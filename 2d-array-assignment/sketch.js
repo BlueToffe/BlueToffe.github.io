6fa200a49decb4ab0d9014022a9469d6e0539ecb
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-03-31 14:16:16 -0600

    changed project type

--- 2d-array-assignment/sketch.js
+++ 2d-array-assignment/sketch.js
@@ -1,10 +1,10 @@
-// Project Title
-// Your Name
+// osu start (2d array assignment & final project)
+// Parker Duggan
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+//512, 384
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
