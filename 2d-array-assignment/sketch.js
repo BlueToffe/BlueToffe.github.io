42fea71bd0815f622fbb608627b03a16ee460ebf
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-04-02 14:15:09 -0600

    created a 2d array to place objects into

--- 2d-array-assignment/sketch.js
+++ 2d-array-assignment/sketch.js
@@ -8,11 +8,46 @@
 // node package manager(NPM)
 //start with making a grid and ahve them appear on that grid 
 
+// simplified 
+// create as smaller square that increases in size when it isthe size of the full square make hte player click
+// if early/late by 50ms give 100 if early/late by 100 ms give 50
+
+let notesToHit = [];
+let rows;
+let cols;
+const CELL_SIZE = 50;
+let grid = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1500, 700);
+  rows = Math.floor(height/CELL_SIZE);
+  cols = Math.floor(width/CELL_SIZE);
+
+  grid = generateGrid(cols, rows); 
 }
 
 function draw() {
   background(220);
+
+  displayClickableGrid();
+}
+
+function displayClickableGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      fill(255);
+      rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    }
+  }
+}
+
+function generateGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++){
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++){ 
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
